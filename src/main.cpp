21b7a36f4a5ded3ecfc1c84583276a26cec2f4f0
Sam <example@ecample.com>
2018-02-15 19:35:44 -0700

    cleaned up code a little

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,6 @@ using namespace std;
 
 Point camera(0,0,40);
 
-Color ambient;
 vector<Solid*> objs;
 int num_objs = 0;
 int num_lights = 0;
@@ -23,16 +22,14 @@ default_random_engine* generators;
 long* num_rays;
 double exposure;
 int num_blurs;
-int pixel_samples;
-int width,height;
+int paths_per_pixel;
+int width;
+int height;
 double scale;
 double lens_radius;
 double focal_length;
-int shadow_samples;
-int ray_depth;
+int max_ray_depth;
 int num_threads;
-int scatter_samples;
-double distance_scale;
 
 void get_intersection(const Line& ray, Material* mat, Vector* normal, Point* p){
     for(int i = 0; i < num_objs; i++){
@@ -45,16 +42,19 @@ void get_intersection(const Line& ray, Material* mat, Vector* normal, Point* p){
     }
 }
 
-Line get_indirect_reflection(const Line& ray, const Point& p, const Vector& normal, const Material& mat, int thread_num){
+//returns a random ray in the hemisphere around point p with with some normal
+//Generates a ray in hemisphere around <0,0,1> and rotates it to proper hemisphere
+Line get_reflection_ray(const Point& p, const Vector& normal, int thread_num){
     Line new_ray(p,Vector());
-    double theta = acos(normal[2]);
+    double theta = acos(normal[2]); //angle between normal and <0,0,1>
     Vector n;
     n[0]=0;n[1]=0;n[2]=1;
-    n=n.cross(normal);
+    n=n.cross(normal); // n is the axis of rotation
     n.normalize();
     double sint = sin(theta);
     double cost = cos(theta);
     Vector v;
+    //the first random variable should be between 0-1 to avoid picking a cosine weighted vector
     double u_rand = (double)generators[thread_num]()/generators[thread_num].max();
     double phi_rand = (double)generators[thread_num]()/generators[thread_num].max()*2*M_PI;
     double sin_rand = sqrt(1-u_rand*u_rand);
@@ -65,29 +65,33 @@ Line get_indirect_reflection(const Line& ray, const Point& p, const Vector& norm
 	new_ray.direction = v;
     }
     else{
+	//rotate the vector to be in correct hemisphere
 	new_ray.direction = cost*(v-n*(n.dot(v))) + n*(n.dot(v)) + sint*n.cross(v);
     }
     return new_ray;
 }			      
 
-Line get_refraction(const Line& ray, const Point& p, const Vector& normal, const Material& mat, double refraction_index, int thread_num){
+//returns a perfectly specular refraction ray
+Line get_refraction_ray(const Line& ray, const Point& p, const Vector& normal, const Material& mat, double refraction_index, int thread_num){
     double n2;
     Vector actual_normal = normal;
-    if (fabs(mat.refraction_index-refraction_index) < 0.0001){ // leaving
+    if (fabs(mat.refraction_index-refraction_index) < 0.0001){ // leaving object
 	n2 = 1;
 	actual_normal = -1*normal;
     }
-    else // entering
+    else // entering object
 	n2 = mat.refraction_index;
     Line ref = ray;
     double nv = actual_normal.dot(ray.direction);
+    //check for total internal reflection
     if((n2*n2)/(refraction_index*refraction_index) < 1-nv*nv) ref.reflect(p,actual_normal);
     else ref.refract(p,actual_normal,refraction_index,n2);
     return ref;
 }
 
+//recursive path building function
+//at each call calculate the next intersection and reflect or refract until limit is reached or path hits a light
 void add_to_path(const Line& ray, int remaining, double refraction_index, int thread_num, Path* path){
-    //cout<<remaining<<endl;
     num_rays[thread_num]++;
     Point p(INFINITY,INFINITY,INFINITY);
     Material mat = {Color(),0,Color(),Color(),0};
@@ -115,21 +119,22 @@ void add_to_path(const Line& ray, int remaining, double refraction_index, int th
 		}
 	    }
 	}
-        Line forward;//=get_indirect_reflection(ray,p,normal,mat,thread_num);
+        Line forward;
 	int change = 1;
 	double n2 = 1;
 	bool reflect = false;
 	if (fabs(mat.refraction_index-refraction_index) < 0.0001){ // leaving
-	    forward = get_refraction(ray,p,normal,mat,refraction_index,thread_num);
+	    forward = get_refraction_ray(ray,p,normal,mat,refraction_index,thread_num);
 	    change = 0;
 	}
 	else{ // entering
+	    //if object isn't at all transparent, don't refract
 	    if (mat.refraction_index == 0){
-		forward = get_indirect_reflection(ray,p,normal,mat,thread_num);
+		forward = get_reflection_ray(p,normal,thread_num);
 		reflect = true;
 	    }
 	    else{
-		forward = get_refraction(ray,p,normal,mat,refraction_index,thread_num);
+		forward = get_refraction_ray(ray,p,normal,mat,refraction_index,thread_num);
 		n2 = mat.refraction_index;
 	    }
 	}
@@ -145,7 +150,9 @@ void add_to_path(const Line& ray, int remaining, double refraction_index, int th
     }
 }
 
+//adds a bloom effect to the image by aplying several gaussian blurs to any pixels that are over exposed
 void blur(Color* colors, int radius, double stddev){
+    //construct gaussian kernel
     double kernel[(2*radius+1)*(2*radius+1)];
     double denom = 2*stddev*stddev;
     for (int y = -radius; y<=radius; y++){
@@ -155,6 +162,7 @@ void blur(Color* colors, int radius, double stddev){
     }
     Color* blurs[1+num_blurs];
     blurs[0] = new Color[width*height];
+    //start with only the pixels that are over exposed. if a pixel intensity is under the exposure there is no blooming
     for (int i = 0; i<width*height; i++){
 	double r,g,b;
 	r=g=b=0;
@@ -163,6 +171,7 @@ void blur(Color* colors, int radius, double stddev){
 	if (colors[i].b*exposure > 1) b = exposure;
 	blurs[0][i] = Color(r,g,b);
     }
+    //apply the gaussian blur several times
     for (int i = 1; i< 1+num_blurs; i++){
 	blurs[i] = new Color[width*height];
 	for (int y = 0; y<height; y++){
@@ -179,14 +188,17 @@ void blur(Color* colors, int radius, double stddev){
 	    }
 	}
     }
+    //add bloom to regular image
     for (int i = 0; i<width*height; i++){
 	colors[i] = colors[i] + blurs[num_blurs][i];
     }
-    for (int i = 1; i<num_blurs+1; i++){
+    //clean up memory
+    for (int i = 0; i<num_blurs+1; i++){
 	delete[] blurs[i];
     }
 }
 
+//convert the array of Colors to the final array of rgb int values
 void expose(int* img, Color* colors){
     blur(colors, 20, 3);
     for (int y=0; y<height; y++){
@@ -196,32 +208,40 @@ void expose(int* img, Color* colors){
     }
 }
 
-int completed = 0;
+//construct a path and then trace back the intensities
+int completed = 0; //keep track of completed pixels to display progres
 mutex completed_lock;
 void do_rays_i(Color* img, int num){
-    int local_count = 0;
+    int local_count = 0; //in case this thread can't aquire the lock, keep going and try again later
+    //each thread does an entire row
     for (int y = num; y < height; y+=num_threads){
 	for (int x = 0; x < width; x++) {
 	    Color c;
-	    for (int i = 0; i<pixel_samples; i++){
+	    for (int i = 0; i<paths_per_pixel; i++){
+		//get a random point inside the pixel
 		double xShift = (double)generators[num]()/generators[num].max()-0.5;
 		double yShift = (double)generators[num]()/generators[num].max()-0.5;
 		Point pixel((double)(x-width/2+xShift)/scale,(double)(height/2-y+yShift)/scale,0);
+		//get a random vector inside the lens
 		double theta = (double)generators[num]()/generators[num].max()*2*M_PI;
 		double rad = (double)generators[num]()/generators[num].max()*lens_radius;
 		Vector lens_shift;
 		lens_shift[0] = rad*cos(theta); lens_shift[1] = rad*sin(theta); lens_shift[2] = 0;
-		Vector D = pixel-camera;
+		Vector D = pixel-camera; //vector to focal point
 		D.normalize();
-		Point C = camera+focal_length*D;
+		Point C = camera+focal_length*D; //focal point
+		//ray from lens point to focal point
 		Line ray = Line(camera+lens_shift, C);
+		//build and trace path
 		Path path(camera+lens_shift);
-		add_to_path(ray,ray_depth,1,num,&path);
+		add_to_path(ray,max_ray_depth,1,num,&path);
 		c = c+ path.trace();
 	    }
-	    c = c/pixel_samples;
+	    //average colors and add to array
+	    c = c/paths_per_pixel;
 	    img[y*width+x]=c;
 	}
+	//try to get lock to update number of pixels completed
 	if (completed_lock.try_lock()){
 	    completed += local_count +1;
 	    completed_lock.unlock();
@@ -230,21 +250,20 @@ void do_rays_i(Color* img, int num){
 	else{
 	    local_count++;
 	}
+	//no need for a lock here because it doesn't matter if another thread writes to completed while printing
 	if (num==0) cout << "\r" << "Progress: " << fixed << setprecision(2) << ((double)(completed)/height)*100 << "%" << flush;
     }
 }
 
 int main(int argc, char** argv){
     objs=*read_json_scene("scene.json");
-    cout << "Scale:          " << scale << endl;
-    cout << "Width:          " << width << endl;
-    cout << "Height:         " << height << endl;
-    cout << "Shadow samples: " << shadow_samples << endl;
-    cout << "Pixel samples:  " << pixel_samples << endl;
-    cout << "Scatter rays:   " << scatter_samples << endl;
-    cout << "Threads:        " << num_threads << endl;
-    cout << "Depth:          " << ray_depth << endl;
-    cout << "Max rays:       " << (double)width*height*pixel_samples*ray_depth*pow(scatter_samples,ray_depth) << endl;
+    cout << "Scale:           " << scale << endl;
+    cout << "Width:           " << width << endl;
+    cout << "Height:          " << height << endl;
+    cout << "Paths per pixel: " << paths_per_pixel << endl;
+    cout << "Threads:         " << num_threads << endl;
+    cout << "Max ray depth:   " << max_ray_depth << endl;
+    cout << "Max rays:        " << (double)width*height*paths_per_pixel*max_ray_depth << endl;
     num_rays = new long[num_threads]();
     generators = new default_random_engine[num_threads];
     int* img = new int[width*height];
--- src/readShape.cpp
+++ src/readShape.cpp
@@ -10,52 +10,36 @@ using namespace std;
 
 using json = nlohmann::json;
 
-extern Color ambient;
 extern int num_lights;
 extern int num_objs;
 extern double exposure;
 extern int num_blurs;
-extern int pixel_samples;
+extern int paths_per_pixel;
 extern int width;
 extern int height;
 extern double scale;
 extern double lens_radius;
 extern double focal_length;
-extern int shadow_samples;
-extern int ray_depth;
-extern int scatter_samples;
+extern int max_ray_depth;
 extern int num_threads;
-extern double distance_scale;
 
 vector<Solid*>* read_json_scene(string filename){
     vector<Solid*>* objs = new vector<Solid*>;
     ifstream scene(filename);
     json details;
     scene>>details;
-    json a = details["ambient"];
     exposure = details["exposure"];
     num_blurs = details["num_blurs"];
-    double r = a[0];
-    double g = a[1];
-    double b = a[2];
-    //r = (1.0/(1-r)-1)/exposure;
-    //g = (1.0/(1-g)-1)/exposure;
-    //b = (1.0/(1-b)-1)/exposure;
-    ambient = Color(r,g,b);
     num_lights = 0;
     num_objs = details["num_objs"];
-    pixel_samples = details["pixel_samples"];
-    //exposure = details["exposure"];
+    paths_per_pixel = details["paths_per_pixel"];
     width = details["width"];
     height = details["height"];
     scale = details["scale"];
     lens_radius = details["lens_radius"];
     focal_length = details["focal_length"];
-    shadow_samples = details["shadow_samples"];
-    ray_depth = details["ray_depth"];
-    scatter_samples = details["scatter_samples"];
+    max_ray_depth = details["max_ray_depth"];
     num_threads = details["num_threads"];
-    distance_scale = details["distance_scale"];
     for(int i = 0; i< num_objs; i++){
 	json obj;
 	scene>>obj;
